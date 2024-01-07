d880c34921f028455351ff84acb13a09919b8646
Kanjanass3 <s6506021632014@email.kmutnb.ac.th>
2024-01-07 19:34:19 +0700

    save

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send('Hello World!');});
+    res.send("Hello World!");
+});
 
-app.listen(port,
-    () =>{
-    console.log(`Example app listenning at http://localhost:${port}`);
-    });
+app.listening(post, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
